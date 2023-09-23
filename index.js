48cbd833efe81ad81c8678325f83f5939df3a407
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2023-09-23 17:35:10 +0600

    mongo config created

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const { MONGO_IP, MONGO_USER, MONGO_PASSWORD, MONGO_PORT } = require("./config/config");
 const app = express();
 const port = process.env.PORT || 4000;
 
 mongoose
-  .connect("mongodb://root:password@mongo:27017/express-mongo") // mongo is the name of the container in docker-compose file, it resolves to the IP address of the container
+  .connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`) // mongo is the name of the container in docker-compose file, it resolves to the IP address of the container
   .then(() => console.log("MongoDB connected"))
-  .catch((err) => console.log(err));
+  .catch((err) =>{
+    console.log("==================");
+    console.log(MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT)
+    console.log("==================");
+    console.log(err);
+  });
 
 app.get("/", (req, res) => {
   res.json({
