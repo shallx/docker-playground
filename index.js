c9de2ed6d49ea7a69a997a68d094d09f337e284f
Rafat Rashid Rahi <rafat.rashid247@gmail.com>
2023-09-24 17:56:46 +0600

    auto reconnect added

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const { MONGO_IP, MONGO_USER, MONGO_PASSWORD, MONGO_PORT } = require("./config/config");
+const {
+  MONGO_IP,
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_PORT,
+} = require("./config/config");
 const app = express();
 const port = process.env.PORT || 4000;
 
-mongoose
-  .connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`) // mongo is the name of the container in docker-compose file, it resolves to the IP address of the container
-  .then(() => console.log("MongoDB connected"))
-  .catch((err) =>{
-    console.log("==================");
-    console.log(MONGO_USER, MONGO_PASSWORD, MONGO_IP, MONGO_PORT)
-    console.log("==================");
-    console.log(err);
-  });
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
+
+const connectWithRetry = () => {
+  mongoose
+    .connect(mongoURL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }) // mongo is the name of the container in docker-compose file, it resolves to the IP address of the container
+    .then(() => console.log("MongoDB connected"))
+    .catch((err) => {
+      console.log(err);
+      setTimeout(connectWithRetry, 5000); // retry after 5 seconds
+    });
+};
+
+connectWithRetry();
 
 app.get("/", (req, res) => {
   res.json({
